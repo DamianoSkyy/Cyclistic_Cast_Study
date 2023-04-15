52a5b448e3409ab1b84f4f8170f157814c65642c
Damian Miesak <105463384+DamianoSkyy@users.noreply.github.com>
2023-04-15 13:52:16 +0100

    Create duration_check.sql

---
+++ duration_check.sql
@@ -0,0 +1,5 @@
+SELECT * 
+FROM `capstone-383520.tripdata.uncleaned_data`
+WHERE
+  ride_total_seconds <= 0
+-- 1,321,112 rows with negative and zero values found for total duration, which can skew the data.
